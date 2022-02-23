1ea340f857d3dba5e5eb4096f4d363cb639edc30
Lev <cheslev01@gmail.com>
2022-02-23 11:55:31 +0300

    Ado Labs from 1 to 6

---
+++ AdoNet_Lab6_ LoadDataSetXml/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNet_Lab6__LoadDataSetXml
+{
+    public partial class Form1 : Form
+    {
+        DataSet NorthwindDataSet = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadSchemaButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataSet.ReadXml("Northwind.xsd");
+            CustomersGrid.DataSource = NorthwindDataSet.Tables["Customers"];
+
+            OrdersGrid.DataSource = NorthwindDataSet.Tables["Orders"];
+
+        }
+
+        private void LoadDataButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataSet.ReadXml("Northwind.xml");
+        }
+    }
+}
